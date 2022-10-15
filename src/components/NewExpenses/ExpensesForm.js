b8d5f4f661698dbe1ecb5100cefc2dd551ac21e4
unknown <andre.lmm91@gmail.com>
2022-10-15 07:49:06 +0200

    adding a slide element and cancel button

--- src/components/NewExpenses/ExpensesForm.js
+++ src/components/NewExpenses/ExpensesForm.js
@@ -80,6 +80,7 @@ function ExpensesForm(props) {
         </div>
       </div>
       <div className="new-expense__action">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpenses.css";
 import ExpensesForm from "./ExpensesForm";
 
 function NewExpenses(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   // retrieving the info from the child ExpenseForm back to here, we create a Handler function and send it as props to the form.
   function saveExpenseDataHandler(enteredExpenseData) {
     // const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
@@ -10,12 +12,21 @@ function NewExpenses(props) {
     // console.log('in NewExpenses.js'); console.log(enteredExpenseData);
     // handler function to send the data back to the parent
     props.onAddExpense(enteredExpenseData);
+    // setIsEditing(false);
+  }
+
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+  function stopEditingHandler() {
+    setIsEditing(false);
   }
 
   return (
     <div className="new-expense">
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
       {/* onSaveExpenseData is a function that will be triggered when the data is saved. we only point to the function, we dont execute it by having () at the end*/}
-      <ExpensesForm onSaveExpenseData={saveExpenseDataHandler} />
+      {isEditing && <ExpensesForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
     </div>
   );
 }
